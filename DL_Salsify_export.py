c4166720741b8984a2cf0c6400205cbd05331c81
t6vmp7 <31556759+t6vmp7@users.noreply.github.com>
2020-04-06 19:17:27 +0800

    update due to Salsify Web new version

--- DL_Salsify_export.py
+++ DL_Salsify_export.py
@@ -6,14 +6,13 @@ Created on Wed Oct 16 09:57:57 2019
 """
 
 import os
-from os.path import exists
+import shutil
 import time
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
 
 # Today's date
 date_label = time.strftime('%Y%m%d')
@@ -47,36 +46,58 @@ prefs = {'download.default_directory' : Download_dir}
 options.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(driver_path,chrome_options=options)
     
-# Supplier Oasis website turn into login page
-Supplier_Oasis = 'https://app.salsify.com/users/sign_in'
-driver.get(Supplier_Oasis)
-LoadingChecker = (By.CSS_SELECTOR, 'body > div > form > button')
-WebDriverWait(driver, 30).until(EC.presence_of_element_located(LoadingChecker))
-driver.find_element_by_css_selector('body > div > form > button').click()
+for i in range(5):
+    try:
+        # Supplier Oasis website turn into login page
+        Supplier_Oasis = 'https://app.salsify.com/users/sign_in'
+        driver.get(Supplier_Oasis)
+        LoadingChecker = (By.CSS_SELECTOR, 'body > div > form > button')
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located(LoadingChecker))
+        driver.find_element_by_css_selector('body > div > form > button').click()
+        
+        LoadingChecker = (By.CSS_SELECTOR, '.login-button')
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located(LoadingChecker))
+        # Input username and password and login
+        driver.find_element_by_id('user_email').send_keys(username)
+        driver.find_element_by_id('user_password').send_keys(password)
+        driver.find_element_by_css_selector('.login-button').click()
+        # time.sleep(30)
+        
+        # Click on products
+        css = 'body > div.ember-view > div > nav > div.ember-view > div > div > div'
+        LoadingChecker = (By.CSS_SELECTOR, css)
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located(LoadingChecker))
+        driver.find_element_by_css_selector(css).click()
+        # time.sleep(10)
+        
+        # Click on View All
+        css = 'body > div > div.ember-view > div.ember-view > div > div.ember-view > a > div'
+        LoadingChecker = (By.CSS_SELECTOR, css)
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located(LoadingChecker))
+        driver.find_element_by_css_selector(css).click()
+        # time.sleep(10)
+        
+        # Click on Actions #ember298 > div > div
+        css = 'body > div.ember-view > div.application-content > div._filtering-advanced-search_ufmyk4.ember-view > div > div:nth-child(2) > div > div > button'
+        LoadingChecker = (By.CSS_SELECTOR, css)
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located(LoadingChecker))
+        driver.find_element_by_css_selector(css).click()
+        # time.sleep(10)
+        
+        # Download All Columns
+        css = 'body > div.ember-view > div.application-content > div._filtering-advanced-search_ufmyk4.ember-view > div > div:nth-child(2) > div > div >  ul > li:nth-child(5) > a'
+        LoadingChecker = (By.CSS_SELECTOR, css) 
+        WebDriverWait(driver, 30).until(EC.presence_of_element_located(LoadingChecker))
+        driver.find_element_by_css_selector(css).click()
+        time.sleep(60)
+        
+        if os.path.exists(Download_dir+'export.xlsx'):
+            break
+        
+    except Exception as e:
+        print(e)
+        driver.refresh()        
 
-LoadingChecker = (By.CSS_SELECTOR, '.login-button')
-WebDriverWait(driver, 30).until(EC.presence_of_element_located(LoadingChecker))
-# Input username and password and login
-driver.find_element_by_id('user_email').send_keys(username)
-driver.find_element_by_id('user_password').send_keys(password)
-driver.find_element_by_css_selector('.login-button').click()
-time.sleep(10)
+driver.quit()
 
-# click on products > View All > Actions >Download All Columns
-driver.find_element_by_css_selector('body > div.ember-view > div > nav > div.ember-view > div > div > div').click()
-time.sleep(3)
-
-driver.find_element_by_css_selector('body > div > div.drop-content > div > div > div > a > div').click()
-time.sleep(3)
-
-driver.find_element_by_css_selector('#content > div > div._header_3hfy1l > div > div > div > button').click()
-time.sleep(3)
-
-driver.find_element_by_css_selector('#content > div > div._header_3hfy1l > div > div > div > ul > li:nth-child(5) > a').click()
-# #ember549 > ul > li:nth-child(5) > a
-time.sleep(60)
-
-if exists('Salsify export_'+date_label+'.xlsx'):
-    os.remove('Salsify export_'+date_label+'.xlsx')
-os.rename('export.xlsx', 'Salsify export_'+date_label+'.xlsx')
-driver.quit() 
+shutil.move('export.xlsx', 'Salsify export_'+date_label+'.xlsx')
